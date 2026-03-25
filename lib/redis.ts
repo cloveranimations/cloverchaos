a9c96ba45cd13b6a87e6a5c063a68dc538f95297
Patricio <patyssandy@gmail.comm>
2026-03-25 12:42:25 -0400

    fix redis connection

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,4 +1,4 @@
 import Redis from 'ioredis';
 
-const redis = new Redis(process.env.REDIS_URL!);
+const redis = new Redis(process.env.REDIS_URL!, { maxRetriesPerRequest: 3 });
 export default redis;
